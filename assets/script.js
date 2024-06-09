b9381a51d1c9f71881fda2c4954711211a430405
Drew Easter <drewheaster@gmail.com>
2024-06-09 00:22:17 -0500

    Added comments to CSS, added Resume/downloadable option

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,6 @@
+// I've added a small amount of JS to run some UI/UX logic, nothing too crazy.
+// The first script lets the page reset to the top after a reload, and the second brings the 
+// "btnScrollToTop" onto the screen after the user scrolls past the first page.
 history.scrollRestoration = 'manual';
 
 const btnScrollToTop = document.querySelector(".btnScrollToTop");
